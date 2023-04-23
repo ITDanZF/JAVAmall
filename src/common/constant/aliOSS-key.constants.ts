874c77b7cf5b64c64fd4a5c6e7ea2e6edf145aeb
何金玉 <18942429812@jin10.co>
2023-04-23 17:03:44 +0800

    隐藏key

--- src/common/constant/aliOSS-key.constants.ts
+++ src/common/constant/aliOSS-key.constants.ts
@@ -1,8 +1,5 @@
 enum AliOSSKeyConstants {
-  AccessKeyID = 'LTAI5tJwxSBGbr4Qa9mxiKbd',
-  AccessKeySecret = 'jwJo996Igj5LL2fx3996esQEPuajKi',
-  region = 'oss-cn-beijing',
-  bucket = 'hejinyu'
+
 }
 
 export default AliOSSKeyConstants
